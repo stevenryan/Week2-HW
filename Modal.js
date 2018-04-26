2ad827f4380857da57e904861ee6609388ea3459
Steven Ng <stevenrng@gmail.com>
2018-04-25 23:49:32 -0400

    Fixed timer

--- Modal.js
+++ Modal.js
@@ -1,7 +1,7 @@
 var modal = document.getElementById("gusherModal");
 var span = document.getElementsByClassName("close")[0];
 
-setTimeout (function() {modal.style.display = "block";}, 1000);
+setTimeout (function() {modal.style.display = "block";}, 3000);
 
 span.onclick = function() {
     modal.style.display = "none";
